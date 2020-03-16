405a7f640fc563329f41d7a1f0e276d4818ef3d9
bharat_patil <patilbharat09@gmail.com>
2020-03-16 14:35:49 +0530

    Added home page screen desing

--- app/screens/Home/index.js
+++ app/screens/Home/index.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { Icon } from 'native-base';
 import styles from './styles';
 
 export default function Login() {
   return (
     <View style={styles.container}>
-      <Text>Home</Text>
+      <View style={styles.uploadHeader}>
+        <Icon style={styles.uploadHeaderIcon} type="FontAwesome" name="file-image-o" />
+        <Text style={styles.uploadHeaderText}>You need to Upload your</Text>
+        <Text style={styles.uploadHeaderSubText}>Electricity Bill</Text>
+      </View>
+      <View style={styles.cameraBlock}>
+        <Icon style={styles.cameraButton} type="FontAwesome" name="camera">  <Text>Use Camera</Text></Icon>
+      </View>
+      <View style={styles.selectDocument}>
+        <Text style={styles.orText}>-------------- or --------------</Text>
+        <View style={styles.selectDocumentBtn}>
+          <Icon style={styles.selectDocumentBtnIcon} type="FontAwesome" name="file-image-o" />
+          <View>
+            <Text style={styles.selectDocumentBtnText}>Select the document from Gallery</Text>
+            <Text style={styles.selectDocumentBtnSubText}>(Only select PNG files)</Text>
+          </View>
+        </View>
+      </View>
     </View>
   );
 }
--- app/screens/Home/styles.js
+++ app/screens/Home/styles.js
@@ -3,9 +3,82 @@ import { StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#FDFDFD',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  uploadHeader: {
+    backgroundColor: '#162A53',
+    flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
+    textAlignVertical: 'center',
+    height: '60%',
+    width: '100%'
   },
+  uploadHeaderIcon: {
+    marginBottom: 20,
+    fontSize: 70,
+    color: "#ffffff" 
+  },
+  uploadHeaderText: {
+    color: '#ffffff',
+    fontSize: 15
+  },
+  uploadHeaderSubText: {
+    color: '#ffffff',
+    fontSize: 30,
+    fontWeight: "bold"
+  },
+  cameraBlock: {
+    marginTop: -30,
+    backgroundColor: '#F79E33',
+    width: '60%',
+    height: 60,
+    borderRadius: 40,
+    alignItems: 'center',
+    textAlignVertical: 'center',
+    justifyContent: 'center',
+  },
+  cameraButton: {
+    fontSize: 18,
+    color: '#ffffff',
+  },
+  selectDocument: {
+    backgroundColor: '#FDFDFD',
+    flexDirection: 'column',
+    alignItems: 'center',
+    height: '40%',
+    width: '100%'
+  },
+  orText: {
+    color: '#cecccc',
+    marginTop: 20
+  },
+  selectDocumentBtn: {
+    width: '90%',
+    borderRadius: 7,
+    marginTop: 50,
+    borderWidth: 1,
+    flexDirection: 'row',
+    borderColor: '#cecccc',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 60
+  },
+  selectDocumentBtnIcon: {
+    paddingRight: 10,
+    fontSize: 30,
+    color: '#162A53'
+  },
+  selectDocumentBtnText: {
+    color: '#162A53',
+    fontSize: 16
+  },
+  selectDocumentBtnSubText: {
+    color: '#cecccc',
+    fontSize: 10
+  }
 });
 
 export default styles;
