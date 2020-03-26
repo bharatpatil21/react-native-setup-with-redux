c4fb9986e437b91560628b26cfb0b96f70dc2288
bharat_patil <patilbharat09@gmail.com>
2020-03-26 10:58:18 +0530

    Added header in image detail page

--- app/navigation/NavigationStack.js
+++ app/navigation/NavigationStack.js
@@ -4,21 +4,32 @@ import { createStackNavigator } from 'react-navigation-stack';
 import Login from '../screens/Login';
 import Home from '../screens/Home';
 import Ocr from '../screens/Ocr';
- 
+
 
 const RNApp = createStackNavigator(
   {
     Login: {
       screen: Login,
+      navigationOptions: {
+        title: 'Login',
+        headerShown: false
+      },
     },
     Home: {
       screen: Home,
+      navigationOptions: {
+        title: 'Home',
+        headerShown: false
+      },
     },
     Ocr: {
       screen: Ocr,
+      navigationOptions: {
+        title: 'Home',
+        headerShown: true
+      },
     },
   },
-  { headerMode: 'none' },
   {
     initialRouteName: 'Login',
   },
